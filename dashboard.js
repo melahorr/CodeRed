c8084cd2f3cb759b910acfc9c6ff2cc72c8b0fe6
melahorr <melapearl@gmail.com>
2018-11-04 06:20:42 -0600

    sdjfklnm

--- dashboard.js
+++ dashboard.js
@@ -33,9 +33,26 @@ listOfPeople.push(personThree);
 listOfPeople.push(personFour);
 listOfPeople.push(personFive);
 
-function addNextPerson()
+function createTable()
 {
-	let elem = document.createElement("ELEMENT");
-	elem.innerHTML = listOfPeople[iterator].name;
-	document.getElementByClass("column").appendChild(elem);
+	let tableData = listOfPeople;
+	var table = document.createElement('table');
+	var tableBody = document.createElement('tbody');
+
+	tableData.forEach(function(rowData)
+	{
+		var row = document.createElement('tr');
+
+		rowData.forEach(function(cellData)
+		{
+			var cell = document.createElement('td');
+			cell.appendChild(document.createTextNode(cellData));
+			row.appendChild(cell);
+		});
+
+		tableBody.appendChild(row);
+	});
+
+	table.appendChild(tableBody);
+	document.body.appendChild(table);
 }
